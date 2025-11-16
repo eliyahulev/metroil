7afad5a7ebc18760aeff310c252d57892cd49d24
Eli Lev <eliyahu.lev@taboola.com>
2025-11-16 16:53:14 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   build: {
+    outDir: ".",
+    emptyOutDir: false,
     rollupOptions: {
       output: {
         manualChunks: undefined,
